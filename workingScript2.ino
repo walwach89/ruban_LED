099d5305045b65383d07beeafb565c68dbe9773f
RobinStGeorges <robin.sgeorges@gmail.com>
2019-01-23 22:00:49 +0100

    Add files via upload

--- workingScript2.ino
+++ workingScript2.ino
@@ -15,14 +15,16 @@ bool led1, led2, led3, led4, led5, led6, led7, led8;
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
+String ledsColors[NUM_LEDS];
 
 String codeVersion = "Version 1.0  Aug 2016 by TonesB";
 
 // WiFi Router Login - change these to your router settings
-const char* SSID = "ESGI";
-const char* password = "Reseau-GES";
+//const char* SSID = "ESGI";
+//const char* password = "Reseau-GES";
 
-
+const char* SSID = "Xana";
+const char* password = "lyoko2468";
 
 // Create the ESP Web Server on port 80
 WiFiServer WebServer(80);
@@ -33,7 +35,7 @@ int cpt = 0;
 void setup() {
 
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  FastLED.setBrightness( 40 );
+  FastLED.setBrightness( 150 );
 
   Serial.begin(115200);
   delay(10);
@@ -90,95 +92,50 @@ void loop() {
   Serial.println(request);
   client.flush();
 
-  // Process the request:
 
-  if (request.indexOf("/led1") != -1 ) {
-    if (led1 == false) {
-      led1 = true;
-      leds[0] = CRGB::Blue;
-    }
-    else {
-      leds[0] = CRGB::Black;
-      led1 = false;
-    }
-  }
-  if (request.indexOf("/led2") != -1 ) {
-    if (led2 == false) {
-      led2 = true;
-      leds[1] = CRGB::Blue;
-    }
-    else {
-      leds[1] = CRGB::Black;
-      led2 = false;
-    }
-  }
-  if (request.indexOf("/led3") != -1 ) {
-    if (led3 == false) {
-      led3 = true;
-      leds[2] = CRGB::Blue;
-    }
-    else {
-      leds[2] = CRGB::Black;
-      led3 = false;
-    }
 
-  }
-  if (request.indexOf("/led4") != -1 ) {
-    if (led4 == false) {
-      led4 = true;
-      leds[3] = CRGB::Blue;
-    }
-    else {
-      leds[3] = CRGB::Black;
-      led4 = false;
-    }
-  }
-  if (request.indexOf("/led5") != -1 ) {
-    if (led5 == false) {
-      led5 = true;
-      leds[4] = CRGB::Blue;
-    }
-    else {
-      leds[4] = CRGB::Black;
-      led5 = false;
-    }
+  // get x, y, value from request
 
-  }
-  if (request.indexOf("/led6") != -1 ) {
-    if (led6 == false) {
-      led6 = true;
-      leds[5] = CRGB::Blue;
-    }
-    else {
-      leds[5] = CRGB::Black;
-      led6 = false;
-    }
+  String xVal = getValue(request, ',', 1);
+  String yVal = getValue(request, ',', 2);
+  String hexVal = getValue(request, ',', 3);
 
-  }
-  if (request.indexOf("/led7") != -1 ) {
-    if (led7 == false) {
-      led7 = true;
-      leds[6] = CRGB::Blue;
-    }
-    else {
-      leds[6] = CRGB::Black;
-      led7 = false;
-    }
+  hexVal = getValue(hexVal, ' ', 0);
 
-  }
-  if (request.indexOf("/led8") != -1 ) {
-    if (led8 == false) {
-      led8 = true;
-      leds[7] = CRGB::Blue;
-    }
-    else {
-      leds[7] = CRGB::Black;
-      led8 = false;
-    }
+  int xvalue = xVal.toInt();
+  int yvalue = yVal.toInt();
 
+
+  String value;
+  value = hexVal;
+
+  String r = hexVal.substring(0, 2);
+  int rInt = hexToDec(r);
+
+  String g = hexVal.substring(2, 4);
+  int gInt = hexToDec(g);
+
+  String b = hexVal.substring(4, 6);
+  int bInt = hexToDec(b);
+
+
+
+  //Put the hex value on x y pos
+  leds[xvalue - 1].setRGB( rInt, gInt, bInt);
+  ledsColors[xvalue - 1] = value ;
+  FastLED.show();
+
+  for (int parc = 0; parc < 8; parc++) {
+    Serial.println(ledsColors[parc]);
   }
 
 
+  if (request.indexOf("/reset") != -1) {
+    for (int cptReset = 0; cptReset < NUM_LEDS ; cptReset ++) {
+      leds[cptReset].setRGB( 0, 0, 0);
+      FastLED.show();
+    }
+  }
 
   // Return the response
   client.println("HTTP/1.1 200 OK");
@@ -193,55 +150,76 @@ void loop() {
 
   client.println("</br></br>");
 
+  client.println("");
 
-  
+  for (int j = 1; j <= 5; j++) {
+    for (int i = 1; i <= NUM_LEDS ; i++) {
+      //
+      //      String url = "<a href=\"/led,";
+      //      url.concat(i);
+      //      url.concat(",");
+      //      url.concat(j);
+      //      url.concat(",");
+      //      url.concat("FFFFFF");
+      //      url.concat("\"> led");
+      //      url.concat(i);
+      //      url.concat("</a>");
+      //
+      //    client.println(url);
+      //
+      //
+      //    client.print("<form action=\"http://");
+      //    client.print(WiFi.localIP());
+      //    client.print("/led1,");
+      //    client.print(i);client.print(",");
+      //    client.print(j);client.print(",");
+      //    String valeurColor;
+      //    if(ledsColors[i] == ""){
+      //       valeurColor = "000000";
+      //    }
+      //    else{
+      //      valeurColor=ledsColors[i];
+      //    }
+      //    client.print(valeurColor);
+      //    client.print("\">");
+      //    client.print(" <input type=\"submit\" value=\"led");
+      //    client.print(i);
+      //    client.print("\" /> ");
+      //    client.print(" </form>");
+      //    }
+      //    client.println("<br>");
 
-    for (int i = 1; i < 30; i++) {
-
-      String url = "<a href=\"/led";
-      url.concat(i);
-      url.concat("\"> led");
-      url.concat(i);
-      url.concat("</a>");
-
-
-
-
-      client.println(url);
+      client.print("<input type=button onclick=window.location.href='http://");
+      client.print(WiFi.localIP());
+      client.print("/led,");
+      client.print(i); client.print(",");
+      client.print(j); client.print(",");
+      String valeurColor;
+      if (ledsColors[i-1] == "") {
+        valeurColor = "000000";
+      }
+      else {
+        valeurColor = ledsColors[i-1];
+      }
+      client.print(valeurColor);
+      client.print("'; style=\"background-color:#");
+      client.print(valeurColor);
+      client.print("\";>");
     }
-  
+    client.print("<br>");
+  }
 
-  Serial.println("led 1 ");
-  Serial.println(led1);
-  Serial.println("<br>");
+  //client.print("<form action=\"http://");
+  //client.print(WiFi.localIP());
+  //client.print("/led1,");
+  //client.print(i);client.print(",");
+  //client.print(j);client.print(",");
+  //client.print(ledsColors[i]);
+  //client.print("\">");
+  //client.print(" <input type=\"submit\" value=\"led\" /> ");
+  //client.print(" </form>");
 
-  Serial.println("led 2 ");
-  Serial.println(led2);
-  Serial.println("<br>");
-
-  Serial.println("led 3 ");
-  Serial.println(led3);
-  Serial.println("<br>");
-
-  Serial.println("led 4 ");
-  Serial.println(led4);
-  Serial.println("<br>");
-
-  Serial.println("led 5 ");
-  Serial.println(led5);
-  Serial.println("<br>");
-
-  Serial.println("led 6 ");
-  Serial.println(led6);
-  Serial.println("<br>");
-
-  Serial.println("led 7 ");
-  Serial.println(led7);
-  Serial.println("<br>");
-
-  Serial.println("led 8 ");
-  Serial.println(led8);
-  Serial.println("<br>");
+  client.println("<a href=\"/reset\">RESET</a>");
 
   client.println("</br>");
 
@@ -252,4 +230,40 @@ void loop() {
 
   Serial.println("");
   FastLED.show();
+}
+
+
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = { 0, -1 };
+  int maxIndex = data.length() - 1;
+
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+int hexToDec(String hexString) {
+
+  unsigned int decValue = 0;
+  int nextInt;
+
+  for (int i = 0; i < hexString.length(); i++) {
+
+    nextInt = int(hexString.charAt(i));
+    if (nextInt >= 48 && nextInt <= 57) nextInt = map(nextInt, 48, 57, 0, 9);
+    if (nextInt >= 65 && nextInt <= 70) nextInt = map(nextInt, 65, 70, 10, 15);
+    if (nextInt >= 97 && nextInt <= 102) nextInt = map(nextInt, 97, 102, 10, 15);
+    nextInt = constrain(nextInt, 0, 15);
+
+    decValue = (decValue * 16) + nextInt;
+  }
+
+  return decValue;
 }
